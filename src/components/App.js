a4c9e06072f7680dd8fe8f79757c9b56968a5930
Steve Murigi <steve.murigi@student.moringaschool.com>
2024-08-29 20:13:45 -0500

    Animal Shelter Lab Completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,33 @@ import React, { useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
+import data from "../db.json";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  function onChangeType(type){
+    setFilters({...filters, type})
+  }
+ 
+
+  function onFindPetsClick(){
+    let listOfPets = data.pets;
+      if(filters.type !== "all") {
+        listOfPets = listOfPets.filter((pet) => pet.type === filters.type);
+      } 
+        setPets(listOfPets)
+    }     
+
+  const onAdoptPet = (id) => {
+    setPets((prevPets) =>
+      prevPets.map((pet) =>
+        pet.id === id ? { ...pet, isAdopted: true } : pet
+      )
+    );
+  };
+  
 
   return (
     <div className="ui container">
@@ -15,10 +38,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType ={onChangeType} onFindPetsClick = {onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets = {pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
